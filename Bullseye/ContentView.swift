d76b4f36cea0270d2bff8a744e79b11b5ddb3b37
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2020-04-24 14:00:19 +0100

    Part 2

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,24 +10,53 @@ import SwiftUI
 
 struct ContentView: View {
   @State var alertIsVisible: Bool = false
+  @State var sliderValue: Double = 50.0
   
   var body: some View {
     VStack {
-      Text("Welcome!")
-        .font(.title)
-        .fontWeight(.semibold)
-        .foregroundColor(Color.orange)
+      Spacer()
+      // Target row
+      HStack {
+        Text("Put the bullseye as close as you can to:")
+        Text("100")
+      }
+      Spacer()
+      // Slider row
+      HStack {
+        Text("1")
+        Slider(value: self.$sliderValue, in:1...100)
+        Text("100")
+      }
+      Spacer()
+      // Button row
       Button(action: {
         self.alertIsVisible = true
       }) {
         Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
           .fontWeight(.medium)
       }
-      .alert(isPresented: $alertIsVisible) {
-        () ->
-          Alert in
-            return Alert(title: Text("Hello there!"), message: Text("This is my first popup"), dismissButton: .default(Text("Awesome")))
+      .alert(isPresented: $alertIsVisible) { () -> Alert in
+        let roundedValue: Int = Int(self.sliderValue.rounded())
+        return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue)."), dismissButton: .default(Text("Play again")))
       }
+      Spacer()
+      // Score row
+      HStack {
+        Button(action: {}) {
+          Text("Start over")
+        }
+        Spacer()
+        Text("Score:")
+        Text("999999")
+        Spacer()
+        Text("Round:")
+        Text("1")
+        Spacer()
+        Button(action: {}) {
+          Text("Info")
+        }
+      }
+      .padding(.bottom, 20)
     }
   }
 }
