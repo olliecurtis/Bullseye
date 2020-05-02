27aa6e250fda15f89ca761bf378668d9fd209460
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2020-05-02 13:48:18 +0100

    Part 4

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -12,14 +12,50 @@ struct ContentView: View {
   @State var alertIsVisible = false
   @State var sliderValue = 50.0
   @State var target = Int.random(in: 1...100)
+  @State var total = 0
+  @State var round = 1
   
   
   func sliderValueRounded() -> Int {
     Int(sliderValue.rounded())
   }
-
+  
+  func amountOff() -> Int {
+    abs(target - sliderValueRounded())
+  }
+  
   func pointsForCurrentRound() -> Int {
-    100 - abs(target - sliderValueRounded())
+    let maximumScore = 100
+    var points = maximumScore - amountOff()
+    let difference = amountOff()
+    if(difference == 0) {
+      points += 100
+    } else if (difference == 1) {
+      points += 50
+    }
+    return points
+  }
+  
+  func alertTitle() -> String {
+    let difference = amountOff()
+    let title: String
+    if difference == 0 {
+      title = "Perfect!"
+    } else if difference < 5 {
+      title = "You almost had it!"
+    } else if difference <= 10 {
+      title = "Not bad."
+    } else {
+      title = "Are you even trying?"
+    }
+    return title
+  }
+  
+  func resetGame() {
+    round = 1
+    total = 0
+    sliderValue = 50.0
+    target = Int.random(in:1...100)
   }
   
   var body: some View {
@@ -46,20 +82,28 @@ struct ContentView: View {
           .fontWeight(.medium)
       }
       .alert(isPresented: $alertIsVisible) { () -> Alert in
-        return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(sliderValueRounded()).\n" + "You scored \(pointsForCurrentRound()) points this round"), dismissButton: .default(Text("Play again")))
+        return Alert(title: Text(alertTitle()),
+               message: Text("The slider's value is \(sliderValueRounded()).\n" + "You scored \(pointsForCurrentRound()) points this round"),
+               dismissButton: .default(Text("Play again")) {
+                  self.total = self.total + self.pointsForCurrentRound()
+                  self.target = Int.random(in:1...100)
+                  self.round += 1
+                })
       }
       Spacer()
       // Score row
       HStack {
-        Button(action: {}) {
+        Button(action: {
+          self.resetGame()
+        }) {
           Text("Start over")
         }
         Spacer()
         Text("Score:")
-        Text("999999")
+        Text("\(total)")
         Spacer()
         Text("Round:")
-        Text("1")
+        Text("\(round)")
         Spacer()
         Button(action: {}) {
           Text("Info")
