e9993a68c38d72393db8f0f05694c2186a32a98a
Oliver Curtis <8831547+olliecurtis@users.noreply.github.com>
2022-05-13 13:40:22 +0100

    V2 of Bullseye with newer swift

---
+++ Bullseye/App/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Oliver Curtis on 28/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ Bullseye/Models/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by Oliver Curtis on 25/02/2022.
+//
+
+import Foundation
+
+struct Game {
+  var target: Int = Int.random(in: 1...100)
+  var score: Int = 0
+  var round: Int = 1
+  
+  func points(sliderValue: Int) -> Int {
+    return 999
+  }
+}
